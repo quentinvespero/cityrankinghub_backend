85a2655da2abcb2f1c3a539c0a1a09255eb7ffb5
Quentin Vespero <quentinvespero@gmail.com>
2024-12-16 03:36:09 +0100

    implementing code database connection

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
 import { configDotenv } from "dotenv"
 import express from 'express'
+import mongoose from "mongoose"
 
 configDotenv()
 
 const app = express()
 
+// middleware
 app.use(express.json())
 
-app.use('api/cityRankingHub')
+// routes
+app.use('/api/cityRankingHub')
+
+// database connection
+const MONGO_URI = process.env.MONGO_URI || 'error : no MONGO_URI given for mongoDB uri'
+
+mongoose
+    .connect(MONGO_URI)
+    .then(() => console.log('connection to mongoDB database OK :)') )
+    .catch((err) => console.error('error while trying to connect to mongoDB',err))
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,5 @@
-const PORT = process.env.PORT || 5001
+import app from "./app"
 
+const PORT = process.env.PORT || 'error : no port given to connect to db'
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
