3f4830e065934bf71005e660d3ea0cef46faa894
Quentin Vespero <quentinvespero@gmail.com>
2025-01-03 19:45:00 +0100

    working on docker compose, to create a specific app_user for the backend

--- database/init.js
+++ database/init.js
@@ -1,12 +1,14 @@
 // this code and the db object come from the initialisation mongoDB shell used in docker-entrypoint-initdb.d
 
-db.createCollection('user')
+db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE)
 
-db.user.insertOne(
-    {
-        username:'quentin',
-        password:'test123',
-        mail:'quentin@gmail.com',
-        subscribedAt: new Date()
-    }
-)
+db.createUser({
+    user: process.env.MONGO_APPUSER_USERNAME,
+    pwd: process.env.MONGO_APPUSER_PASSWORD,
+    roles: [
+        {
+            role: "readWrite",
+            db: process.env.MONGO_INITDB_DATABASE
+        }
+    ]
+})
