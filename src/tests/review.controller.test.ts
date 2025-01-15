992f4a38fec7037abf61269cc70e9e9c9d1eb230
Quentin Vespero <quentinvespero@gmail.com>
2025-01-15 01:24:16 +0100

    working on review test

--- src/tests/review.controller.test.ts
+++ src/tests/review.controller.test.ts
@@ -42,10 +42,13 @@ describe('Review Controller', () => {
         it('should create a new review', async () => {
             const req = { body: requestBody } as Request
             const res = {
-                // creating a Vitest mock function with fn()
-                status: vi.fn().mockReturnThis(),
+                // below, the properties "status" and "json" are indicated as functions, using vi.fn()
+                // so vi.fn() will create mock function, using the name given to the property
+                // (in the controller, these functions looks like that : status().json() )
+                // example : status() --> status: vi.fn()
+                status: vi.fn().mockReturnThis(), // here, since in the controller the status() method return the Response itself, we are doing the same here, while returning This
                 json: vi.fn(),
-            } as unknown as Response
+            } as unknown as Response // casting it as "unknown" first here, to avoid typescript error since before being executed, it isn't of type Response yet
         })
     })
 })
