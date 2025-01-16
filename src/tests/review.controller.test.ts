1258de00575220e1c941475a172ab6fdc795b6b6
Quentin Vespero <quentinvespero@gmail.com>
2025-01-16 19:37:56 +0100

    working on tests for Review

--- src/tests/review.controller.test.ts
+++ src/tests/review.controller.test.ts
@@ -1,5 +1,7 @@
 import { Request } from 'express'
-import { describe, it, vi } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
+import Review from '../models/review.model'
+import * as reviewController from '../controllers/review.controller'
 
 // dummy request body to use in test
 const requestBody = {
@@ -39,6 +41,7 @@ describe('Review Controller', () => {
 
     // create review function testing
     describe('createReview', () => {
+
         it('should create a new review', async () => {
             const req = { body: requestBody } as Request
             const res = {
@@ -49,6 +52,37 @@ describe('Review Controller', () => {
                 status: vi.fn().mockReturnThis(), // here, since in the controller the status() method return the Response itself, we are doing the same here, while returning This
                 json: vi.fn(),
             } as unknown as Response // casting it as "unknown" first here, to avoid typescript error since before being executed, it isn't of type Response yet
+
+            // we use the mongoose method prototype, to create a mock of the model Review, that we save
+            // the whole expression is between brackets to make it all of type any
+            // then we use the method mockResolvedValue to send the request
+            (Review.prototype.save as any).mockResolvedValue(req.body)
+
+            // we call the function createReview, and give it the req and res
+            await reviewController.createReview(req, res)
+
+            // expect is a method that check whether an expected condition is true. Otherwise, the tests will fail
+            // 
+            expect(res.status).toHaveBeenCalledWith(201)
+            expect(res.json).toHaveBeenCalledWith(req.body)
+        })
+
+        it('should return an error if review creation fails', async () => {
+            const req = { body: {} } as Request
+            const res = {
+                status: vi.fn().mockReturnThis(),
+                json: vi.fn(),
+            } as unknown as Response
+
+            (Review.prototype.save as any).mockRejectedValue(new Error('Failed to save'))
+
+            await reviewController.createReview(req, res)
+
+            expect(res.status).toHaveBeenCalledWith(500)
+            expect(res.json).toHaveBeenCalledWith({
+                message: 'Error creating review',
+                error: new Error('Failed to save'),
+            })
         })
     })
 })
