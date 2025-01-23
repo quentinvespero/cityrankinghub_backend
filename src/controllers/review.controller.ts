5f1941ae51b0dd3c5f5a61573638c6328fad3b47
Quentin Vespero <quentinvespero@gmail.com>
2025-01-23 19:44:42 +0100

    review controller should be good for V1

--- src/controllers/review.controller.ts
+++ src/controllers/review.controller.ts
@@ -32,15 +32,28 @@ export const createReview = async (req: Request, res: Response) => {
         city.totalReviews += 1
 
         // update averageRatings
-        // below, for each properties in review.ratings
         for (const propertyName in review.ratings) {
             if (propertyName in city.averageRatings) {
-                
+                // we use the review's value to update the value of the corresponding property in city.averageRatings
+                city.averageRatings[propertyName as keyof typeof city.averageRatings] = 
+                    (city.averageRatings[propertyName as keyof typeof city.averageRatings] * (city.totalReviews -1) + review.ratings[propertyName as keyof typeof review.ratings]) / city.totalReviews
             }
-            // we use the review's value to update the value of the corresponding property in city.averageRatings
-            city.averageRatings[propertyName] = (city.averageRatings[propertyName] * (city.totalReviews - 1) + review.ratings[propertyName]) / city.totalReviews
         }
 
+        // update city.globalRating
+        city.globalRating = 
+            // Object.values() transform the object city.averageRatings into an array of values
+            Object.values(city.averageRatings).reduce((sum,rating) => sum + rating, 0)
+            / 
+            Object.keys(city.averageRatings).length
+        
+        await city.save({session})
+
+        // commiting the transaction, submitting it to the database, if every operations are going well
+        await session.commitTransaction()
+        session.endSession()
+
+        // send the response
         res.status(201).json(review)
     }
     catch (error) {
@@ -48,16 +61,20 @@ export const createReview = async (req: Request, res: Response) => {
     }
 }
 
-// get the reviews for a city
+// get all the reviews submitted for a given city
 // the request will have to provide the cityId
-export const getReviewsOfCity = async (req: Request, res: Response) => {
+export const getCityReviews = async (req: Request, res: Response) => {
     try {
-        const cityId = req.params.cityId // getting cityId from the request
-        const reviewsOfTheCity = await Review.find({ cityId: cityId }) // retrieving the documents in Review collection, that have the same cityId value as the one provided by the request
+        const { cityId } = req.params // Extract cityId from request parameters
 
-        res.status(200).json(reviewsOfTheCity)
+        if (!cityId) return res.status(400).json({ message: 'City ID is required' }) // Validate input
+
+        const cityReviews = await Review.find({ cityId }) // Query reviews by cityId
+
+        res.status(200).json(cityReviews) // Respond with the found reviews
     }
     catch (error) {
-        res.status(500).json({ message: 'error fetching reviews...', error: error })
+        console.error('Error fetching city reviews:', error) // Log the error for debugging
+        res.status(500).json({ message: 'Error fetching reviews', error: error }) // Return error details
     }
 }
